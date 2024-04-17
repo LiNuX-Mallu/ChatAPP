e97e6a77fec473dfb1fb3bcfd427ece97aa30cec
LiNuX-Mallu <shaspachu457@gmail.com>
2024-04-17 10:56:17 +0530

    url fix

--- client/src/constants/urls.ts
+++ client/src/constants/urls.ts
@@ -1,2 +1,5 @@
-export const server = import.meta.env.VITE_SERVER_URL;
-export const api = server+'/api';
+// export const server = import.meta.env.VITE_SERVER_URL;
+// export const api = server+'/api';
+
+export const server = "https://chatapp-wlohst6uqa-de.a.run.app";
+export const api = "https://chatapp-wlohst6uqa-de.a.run.app/api";
